57b5abc0103293af84b365eacd1d6f379e8a74bc
yankai zhang <50317782+walkerlemon@users.noreply.github.com>
2022-08-18 11:08:58 +0800

    Add files via upload

--- Exercise_2/read_tsp.py
+++ Exercise_2/read_tsp.py
@@ -1,16 +1,17 @@
 import tsplib95 as tsp
 
 
-def get_data(path):
+def get_data(path: str):
     problem = tsp.loaders.load(path)
     city_id = list(problem.get_nodes())
-    nodes_coord = []
+    nodes_coord = []  # 城市坐标
     for i in range(1, city_id.__len__() + 1):
         nodes_coord.append(problem.node_coords[i])
 
     return city_id, nodes_coord
 
 
-a, b = get_data('rl1889.tsp')
+a, b = get_data('a280.tsp')
 print(a)
 print(b)
+
