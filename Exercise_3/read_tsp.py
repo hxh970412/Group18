ae858a0110cad79d0697ee46e5f45549dac45e86
pengyu-adelaide <80448725+pengyu-adelaide@users.noreply.github.com>
2022-08-18 17:55:38 +0800

    Add files via upload

---
+++ Exercise_3/read_tsp.py
@@ -0,0 +1,16 @@
+import tsplib95 as tsp
+
+
+def get_data(path):
+    problem = tsp.loaders.load(path)
+    city_id = list(problem.get_nodes())
+    nodes_coord = []
+    for i in range(1, city_id.__len__() + 1):
+        nodes_coord.append(problem.node_coords[i])
+
+    return city_id, nodes_coord
+
+
+a, b = get_data('a280.tsp')
+print(a)
+print(b)
